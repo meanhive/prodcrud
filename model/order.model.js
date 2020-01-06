3dd5acc2f99de0aef06d3e7774af46b44197b1f2
MEAN Hive <meanhive@zoho.com>
2020-01-06 12:46:04 +0530

    Create order.model.js

---
+++ model/order.model.js
@@ -0,0 +1,22 @@
+// db schema
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// declaring collection schema property and names
+
+let Order = new Schema(
+  {
+    // schema property: values
+    username: { type: String },
+    address: { type: String },
+    mobile: { type: String },
+    orderinfo: { type: Array },
+    orderdate: { type: Date }
+  },
+  {
+    collection: 'order'
+  }
+);
+
+module.exports = mongoose.model('Order',Order);
